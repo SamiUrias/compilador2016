e3ee3fda332d4428d5701166f4b275c63b53ac4c
Moisess <soyamigodejesus@hotmail.com>
2015-08-18 11:17:21 -0600

    Segundo commit con el eClosure casi terminado

--- NetBeansProjects/Compilador/src/laboratorio/pkg2/OpExtra.java
+++ NetBeansProjects/Compilador/src/laboratorio/pkg2/OpExtra.java
@@ -5,6 +5,8 @@
  */
 package laboratorio.pkg2;
 
+import java.util.ArrayList;
+
 /**
  * Esta clase tiene operaciones adicionales que son de utilidad al programa
  * principal
@@ -15,17 +17,48 @@ public class OpExtra {
     /**
      * Realiza un eClosure en base a un subset
      * @param subset 
+     * @return  subset
      */
-    public Subset eClosure(Subset subset)
+    public static Subset eClosure(Subset subset)
     {
-        /*Recorre todas las transiciones del subset*/
-        for (int i=0;i<subset.getTransiciones().size();i++)
+        System.out.println("Dentro del eClosure");
+        ArrayList<Integer> yaEClosure = new ArrayList<Integer>();
+        ArrayList<Integer> noEClosure = new ArrayList<Integer>();
+        
+        /*Recorre todas las nodos del subset*/
+        for (int i=0;i<subset.getNodos().size();i++)
         {
-            
-            Transicion trans = subset.getTransiciones().get(i);
-            
-            if (trans.getNodoInicial()==)
+            /*Toma el nodo actual del ciclo*/
+            int nodo = subset.getNodos().get(i);
+            System.out.println(nodo);
+            /*Se recorren todas las transiciones del subset para saber si alguna
+               de esas transiciones corresponde al nodo seleccionado*/
+            System.out.println("La cantidad de transiciones es: " + subset.getTransiciones().size());
+            for (int j=0;j<subset.getTransiciones().size();j++)
+            {
+                System.out.println("T("+subset.transiciones.get(j)+")");
+                /*Si la el nodo corresponde al nodo inicial de la transicion,
+                   entonces se añade el nodo final de la transicion al subset
+                   y se realiza un */
+                if (nodo==subset.getTransiciones().get(j).getNodoInicial())
+                {
+                   if (subset.getTransiciones().get(j).getSimbolo().equals("!"))
+                   {
+                       System.out.println("Se ha encontrado una transicion con epsilon");
+                       /*Se añade el nodo ennconrado al subset*/
+                       subset.add(subset.getTransiciones().get(j).getNodoFinal());
+                       
+                       /*Se hace un e-Closure del nodo encontrado*/
+                       //eClosure(subset.getTransiciones().get(j).getNodoFinal())
+                       
+                       
+                       
+                   }
+                }
+            }
         }
+        
+        return subset;
     }
     
     
