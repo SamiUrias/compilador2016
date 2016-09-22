494b5352d0989e412f73c71aca1afc6392539367
Samuel <Samuel@192.168.34.2>
2016-09-22 01:54:34 -0600

    Se creo la clase Lexer2

---
+++ NetBeansProjects/Compilador/src/lexer/Lexer2.java
@@ -0,0 +1,16 @@
+package lexer;
+
+/**
+ * Esta clase Lexer2 es la clase que se utiliza para hacer el analisis lexico 
+ *  para la fase No.2 del proyecto. 
+ * @author Samuel on 22/09/2016
+ */
+public class Lexer2 {
+    
+    /**
+     * Constructor del Lexer2
+     */
+    public Lexer2(){
+        
+    }
+}
