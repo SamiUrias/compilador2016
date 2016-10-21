f8d481bf3d1f7a679cfd67bcdc6bb9393f54b197
Samuel <Samuel@192.168.1.7>
2016-10-20 23:24:56 -0600

    Se crearon nuevas pruebas unitarias para el IdentAutomata Se puede decir que el IdentAutomata funciona correctamente

--- NetBeansProjects/Compilador/test/lexer/BasicAutomatas/IdentAutomataTest.java
+++ NetBeansProjects/Compilador/test/lexer/BasicAutomatas/IdentAutomataTest.java
@@ -37,24 +37,125 @@ public class IdentAutomataTest {
         assertEquals(expResult, result);
     }
     
-     @Test
+    @Test
     public void identAutomataTest2() {
         System.out.println("Ident Automata test 2");
         boolean expResult = true;
         SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
-        boolean result = simuladorAfn.hacerSimulacion("1");
+        boolean result = simuladorAfn.hacerSimulacion("2");
         assertEquals(expResult, result);
     }
     
-     @Test
+    @Test
     public void identAutomataTest3() {
         System.out.println("Ident Automata test 3");
         boolean expResult = true;
         SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
-        boolean result = simuladorAfn.hacerSimulacion("1");
+        boolean result = simuladorAfn.hacerSimulacion("3");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest4() {
+        System.out.println("Ident Automata test 4");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("a");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest5() {
+        System.out.println("Ident Automata test 5");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("b");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest6() {
+        System.out.println("Ident Automata test 6");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("abc");
         assertEquals(expResult, result);
     }
 
+     
+    @Test
+    public void identAutomataTest7() {
+        System.out.println("Ident Automata test 7");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("abc1");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest8() {
+        System.out.println("Ident Automata test 8");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("abc123");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest9() {
+        System.out.println("Ident Automata test 9");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("123abc123");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest10() {
+        System.out.println("Ident Automata test 10");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("123ABC123");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest11() {
+        System.out.println("Ident Automata test 11");
+        boolean expResult = true;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("123ABCabc");
+        assertEquals(expResult, result);
+    }
+    
+    
+    @Test
+    public void identAutomataTest12() {
+        System.out.println("Ident Automata test 12");
+        boolean expResult = false;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("_123ABCabc");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest13() {
+        System.out.println("Ident Automata test 13");
+        boolean expResult = false;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion(".123ABCabc");
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void identAutomataTest14() {
+        System.out.println("Ident Automata test 14");
+        boolean expResult = false;
+        SimuladorAFN simuladorAfn = new SimuladorAFN(this.identAutomata);
+        boolean result = simuladorAfn.hacerSimulacion("?123ABCabc");
+        assertEquals(expResult, result);
+    }
+    
     /**
      * Test of getAutomata method, of class IdentAutomata.
      */
